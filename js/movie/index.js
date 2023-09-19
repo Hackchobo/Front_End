8cfbf54e2a81b133a9203ffe9fdc9cf8c79cc9b6
Hackchobo <130716383+Hackchobo@users.noreply.github.com>
2023-09-19 18:05:01 +0900

    영화 정렬

---
+++ js/movie/index.js
@@ -0,0 +1,47 @@
+(function(){
+    const accessToken= 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NzJjNmIwYjlhYWQxMzEyYzlkYWY3MmZmZWYzYmM3MiIsInN1YiI6IjYxZmNlOGY3N2E5N2FiMDA0N2U2ZTMzMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Skjv6dYxa7qjdxWSz1tJVMH151T2WMNp4pzuursIeYw'
+    const url = `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=`;
+    const posterPrefix = 'https://image.tmdb.org/t/p/w300';
+
+    let page = 1;
+
+    async function getMovieData() {
+        const res = await fetch(url + page++, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                accept : 'application/json'
+            }
+        });
+
+        const data = await res.json();
+        console.log(data);
+    }
+    getMovieData();
+
+    function makeList(data) {
+        data.forEach(function(item){
+            const li = makeItem(item);
+
+        });
+    }
+
+    function makeItem(item) {
+        const li = document.createElement('li');
+        const imgUrl = posterPrefix + item.poster_path;
+        const cardName = item.original_title;
+        const cardDate = item.release_date;
+        // li.classList.add('listing-card__item');
+        // li.classList.remove('listing-card__item');
+        // li.classList.toggle('listing-card__item');
+        li.className = 'listing-card__item';
+        li.innerHTML = `
+            <div class="listing-card__image" style="background-image:url('${imgUrl}')"></div>
+            <div class="listing-card__info">
+                <strong class="listing-card__name">${cardName}</strong>
+                <p class="listing-card__date">${cardDate}</p>
+            </div>
+        `;
+        return li;
+    }
+
+})();
